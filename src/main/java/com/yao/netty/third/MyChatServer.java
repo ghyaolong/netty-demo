2f359d4cc93655c3351888a891d7d4fda56ff628
tututu <289911401@qq.com>
2019-09-21 18:33:59 +0800

    增加NIO练习

---
+++ src/main/java/com/yao/netty/third/MyChatServer.java
@@ -0,0 +1,29 @@
+package com.yao.netty.third;
+
+import com.yao.netty.second.MyServerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class MyChatServer {
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = null;
+        EventLoopGroup workerGroup = null;
+        try {
+            bossGroup = new NioEventLoopGroup();
+            workerGroup = new NioEventLoopGroup();
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new MyChatServerInitializer());
+            ChannelFuture sync = serverBootstrap.bind(9999).sync();
+            sync.channel().closeFuture().sync();
+        }catch (Exception e){
+
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
