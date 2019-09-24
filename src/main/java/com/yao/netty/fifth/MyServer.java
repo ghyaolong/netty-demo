4b40a80ae1198cc4e6acd460c3089bc652d0722d
tututu <289911401@qq.com>
2019-09-24 16:23:26 +0800

    练习WebSocket

---
+++ src/main/java/com/yao/netty/fifth/MyServer.java
@@ -0,0 +1,31 @@
+package com.yao.netty.fifth;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class MyServer {
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = null;
+        EventLoopGroup workerGroup = null;
+        try {
+            bossGroup = new NioEventLoopGroup();
+            workerGroup = new NioEventLoopGroup();
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new WebSocketChannelInitializer());
+            ChannelFuture sync = serverBootstrap.bind(9999).sync();
+            sync.channel().closeFuture().sync();
+        }catch (Exception e){
+
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/yao/netty/fifth/TextWebSocketFrameHandler.java
@@ -0,0 +1,15 @@
+package com.yao.netty.fifth;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+
+import java.time.LocalDateTime;
+
+public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        System.out.println("接收到客户端的数据："+ msg.text());
+        ctx.channel().writeAndFlush(new TextWebSocketFrame("服务器时间："+ LocalDateTime.now()));
+    }
+}
